628eb7e076e9c6b2368ffae2827a9b7163b995e0
DarshitaBhagat <darshitabhagat54@gmail.com>
2026-06-22 13:57:51 +0530

    calculator

---
+++ calculator/index.js
@@ -0,0 +1,9 @@
+/*
+const age = document.getElementById("age").value;
+const btn = document.getElementById("check");
+const result = document.getElementById("status");
+btn.onclick = function(){
+    const age = document.getElementById("age").value;
+
+*/
+
