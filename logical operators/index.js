95719cf7a0cc310e36d11e0fd41cdddc32e4aba7
DarshitaBhagat <darshitabhagat54@gmail.com>
2026-06-22 14:09:19 +0530

    logical operators

---
+++ logical operators/index.js
@@ -0,0 +1,21 @@
+// = assignment
+// == comparison
+// === strict equality (value and datatype)
+// != not equal
+// !== strict not equal
+// && and
+// || or
+// ! not
+
+
+
+const temp = 20;
+if (temp>0 && temp < 30){
+    console.log("The weather is good");
+}
+else if(temp <= 0 || temp >= 30){
+    console.log("The weather is bad");
+}
+else if (temp != 30 || temp == 0){
+    console.log("weather is weathering");
+}
