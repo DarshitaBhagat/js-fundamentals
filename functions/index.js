54cf5dff1683eaedc145bdb07ce55007e249a429
DarshitaBhagat <darshitabhagat54@gmail.com>
2026-06-22 14:05:27 +0530

    functions

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+function hello(name, day){
+    console.log(`hello ${name}`);
+    console.log("good morning");
+    console.log(`have a good ${day} :)`);
+
+}
+
+
+
+hello("darshita", "monday");hello("bhagat", "saturday");
+
+
+function add(num1, num2){
+    result = num1 + num2;
+    return result;
+}
+
+let ans = add(2, 3);
+console.log(ans);
+
