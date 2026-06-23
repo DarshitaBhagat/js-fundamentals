26432b84347d2d7e7ae5199c5b4554404ede5905
DarshitaBhagat <darshitabhagat54@gmail.com>
2026-06-23 14:34:43 +0530

    while for

---
+++ while for/index.js
@@ -0,0 +1,19 @@
+/*let user = "darshita";
+count = 0;
+do {
+    console.log("hello");
+}
+while (user.indexOf("a") < 8) {
+    console.log("short");
+}
+*/
+const name = "darshita";
+for (let i = 0;i<3;i++){
+    const ele = name[i];
+    console.log(ele);
+}
+// output : d a r
+
+// continue will skip that iteration and move to the next one
+// break will break the loop and move to the next line of code after the loop
+
