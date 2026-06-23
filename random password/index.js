bf0c07e44e6fb00059cb321f91c24938d6a6b91e
DarshitaBhagat <darshitabhagat54@gmail.com>
2026-06-23 14:43:40 +0530

    changes

--- random password/index.js
+++ random password/index.js
@@ -10,6 +10,28 @@ function generatePassword(length, uppercase, lowercase, numbers, symbols) {
     const numberChars = "0123456789";
     const symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
+    let password = "";
+    let allowedChars = "";
+    
+    if (uppercase) {
+        allowedChars += uppercaseChars;
+    }
+    if (lowercase) {
+        allowedChars += lowercaseChars;
+    }
+    if (numbers) {
+        allowedChars += numberChars;
+    }
+    if (symbols) {
+        allowedChars += symbolChars;
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
 }
 
 const password = generatePassword(passlen, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
